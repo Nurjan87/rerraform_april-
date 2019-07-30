2c2cc290dadac3ed5559a23ef41c84f0010b7834
Dastan Kadyrkulov <dastankadyrkulov@Dastans-MacBook-Pro.local>
2019-07-30 09:48:41 -0500

    changin region test

--- instancce.tf
+++ instancce.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-        region = "us-east-1"
+        region = "us-west-1"
 }
-resource "aws_instance" "web1" {
+resource "aws_instance"  {
     ami = "ami-0d8f6eb4f641ef691"
     instance_type = "t2.micro"
 }
