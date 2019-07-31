86416852e0a262da211a0a1413b68a127efa5596
Dastan Kadyrkulov <dastankadyrkulov@Dastans-MacBook-Pro.local>
2019-07-31 11:37:28 -0500

    adding sec_group

--- key_pair.tf
+++ key_pair.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "terraform_april"{
     key_name = "terraform_april"
-    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDPtmo0juBNypAxdHDWGYkbr+AjmkTii8HAARPPJSRUW8Yp7hh8Pe2mT4I7LUQnhLEn40s/rAZJDeW2u9iuE27U9MqmH2mARlkG35AMo3Y3tr0Fud2Gsjp5ZL42OFDTAWolNfYObqJaZL+NU3g2Ob0xBM60ls8Ljzm2Pi1gKRg8w0gDOSXQX+VqyCIFuc24I0gjvpxP96zg60EPWs4Ao+D8TEELWZ4HIp8bEPFWZMxJeWOEeaycLCeYUQ/8YiYIDgtUKId72GAIUjtUXnxIteSeryPF2i30H+Vj+JpAc3uVOkE9rLIOX9Yuk618KVUkhxcg/30UDWqj2/2TwswxZM3D root@ip-172-31-18-220.us-east-2.compute.internal"
+    public_key = "${file("~/.ssh/id_rsa.pub)}"
 }
---
+++ sec_group.tf
@@ -0,0 +1,15 @@
+resource "aws_security_group" "sec1" {
+  name        = "sec1"
+  description = "Allow SSH"
+  vpc_id      = "vpc-05506662"
+
+  ingress {
+    # TLS (change to whatever ports you need)
+    from_port   = 22
+    to_port     = 22
+    protocol    = "-1"
+    # Please restrict your ingress to only necessary IPs and ports.
+    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    cidr_blocks = "0.0.0.0/0"
+  }
+}
