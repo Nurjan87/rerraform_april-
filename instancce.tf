80f9c314f86a8688de7bf85e9796fc96c7e4c617
Dastan Kadyrkulov <dastankadyrkulov@Dastans-MacBook-Pro.local>
2019-07-31 12:46:12 -0500

    testing

--- instancce.tf
+++ instancce.tf
@@ -1,5 +1,13 @@
 resource "aws_instance" "web" {
     ami = "ami-074e2d6769f445be5"
     instance_type = "t2.micro"
-    key_name = "keyforterraform"
+    key_name = "${aws_key_pair.terraform_april.key_name}"
+    security_groups = ["${aws_security_group.sec1.name}"]
+
+    tags = {
+        Name = "Dev"
+        Dept = "IT"
+        Group = "April"
+        Created_by = "Nurjan"
+    }
 }
