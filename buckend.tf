65aa23144b31fe97d9d5926ccfcaf390c97cbd30
Dastan Kadyrkulov <dastankadyrkulov@Dastans-MacBook-Pro.local>
2019-07-31 20:38:00 -0500

    test

--- buckend.tf
+++ buckend.tf
@@ -1,6 +1,5 @@
 terraform {
     backend "s3"{
-        dynamodb_table = "dev_state_lock"
         bucket = "terraform-april-2019-nurjan"
         region = "us-east-1"
         key  = "dev.tfstate"
---
+++ dev.tfvars
@@ -0,0 +1,2 @@
+ami           = "ami-074e2d6769f445be5"
+instance_type = "t2.micro"
