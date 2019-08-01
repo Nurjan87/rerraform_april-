b292665bbf491869f335bea4a9d8e63e254fc108
Dastan Kadyrkulov <dastankadyrkulov@Dastans-MacBook-Pro.local>
2019-07-31 20:26:15 -0500

    changing region

--- buckend.tf
+++ buckend.tf
@@ -2,7 +2,7 @@ terraform {
     backend "s3"{
         dynamodb_table = "dev_state_lock"
         bucket = "terraform-april-2019-nurjan"
-        region = "us-west-1"
+        region = "us-east-1"
         key  = "dev.tfstate"
     }
 }
